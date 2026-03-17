9d38538a0f90405a8c032af329af5017205caca0
droid <kyk151931@gmail.com>
2026-03-17 16:32:54 +0900

    graph.py 변경 push test

--- backend/app/services/graph.py
+++ backend/app/services/graph.py
@@ -4,6 +4,7 @@ import structlog
 from gremlin_python.driver import client, serializer
 from gremlin_python.structure.graph import Graph
 from gremlin_python.driver.driver_remote_connection import DriverRemoteConnection
+from gremlin_python.process.graph_traversal import __
 
 logger = structlog.get_logger()
 
